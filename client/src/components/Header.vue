0c6915aeb4eef96e692b25b2df28e777f2ea966e
Yin TAN <yin.tan@imt-atlantique.net>
2021-12-29 13:04:30 +0100

    update post function

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,36 +1,90 @@
 <template>
     <div class="header">
         <img src="../assets/IMT_Atlantique_logo_RVB.svg" alt="logo"/>
-        <span>
+        <span class="header_text">
             Simple Forum
         </span>
+        <span style="float: right" class="header_text">
+            <el-button type="text" @click="dialogVisible = true" style="padding-top: 0">Post</el-button>
+        </span>
+        <el-dialog
+                title="Post"
+                :visible.sync="dialogVisible"
+                width="30%"
+                :before-close="handleClose">
+            <el-form ref="form" :model="form" label-width="80px">
+                <el-form-item label="Title">
+                    <el-input v-model="form.title"></el-input>
+                </el-form-item>
+                <el-form-item label="Content">
+                    <el-input type="textarea" v-model="form.desc"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="closePost">Cancer</el-button>
+                <el-button type="primary" @click="closePost">Confirm</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Header"
+        name: "Header",
+        data() {
+            return {
+                dialogVisible: false,
+                form: {
+                    title: '',
+                    desc: ''
+                }
+            };
+        },
+        methods: {
+            emptyPost() {
+                this.form.title = '';
+                this.form.desc = '';
+            },
+            closePost() {
+                this.dialogVisible = false;
+                this.emptyPost();
+            },
+            handleClose(done) {
+                this.$confirm('Are you sure to quit post ?')
+                    .then(_ => {
+                        console.log(_)
+                        done();
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    });
+                this.emptyPost();
+            }
+        }
     }
 </script>
 
 <style scoped>
-    .header{
+    .header {
         height: 100%;
         width: 100%;
-        align-items:center;
-        display:-webkit-flex;
+        align-items: center;
+        padding-right: 2vw;
+        box-sizing: border-box;
     }
 
-    img{
+    img {
         height: 100%;
         width: 10vw;
         float: left;
     }
 
-    span{
+    span.header_text {
         text-align: left;
         display: inline-block;
         float: left;
+        align-items: center;
+        padding-top: 2vw;
     }
 
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import locale from 'element-ui/lib/locale/lang/en';
 import App from './App.vue'
 import axios from "axios";
 import VueAxios from "vue-axios";
 import Vuex from 'vuex';
 import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(ElementUI, { locale });
 Vue.use(VueAxios, axios);
 Vue.use(Vuex);
 
